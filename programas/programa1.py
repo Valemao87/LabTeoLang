06afdc8288be2c34c16ef099df9a234dbcccae69
Valemao87 <43651711+Valemao87@users.noreply.github.com>
2026-04-08 15:11:43 -0300

    Add files via upload

---
+++ programas/programa1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import re
+import sys
+from pypdf import PdfReader
+
+def programa1(RutaPdf): 
+
+    #Sets pdf as the pdf content in RutaPdf using PdfReader fucntion thing
+      
+    pdf = PdfReader(RutaPdf)
+
+    #Iterate trough the pages in pdf and append (concatenacion) them in text variable
+    # the function extract_text returns either text or "" if no text was found, thats why the "or" is needed 
+    
+    text = "" # inicializacion de la variable text como string 
+
+    for page in pdf.pages: 
+        text += page.extract_text()
+    
+    
+    return text
+
+
+if __name__ == '__main__':
+    entrada = sys.argv[1]  # archivo entrada (param)
+    salida = sys.argv[2]   # archivo salida (param)
+    
+    ret = programa1(entrada)      # ejecutar 
+    
+    f = open(salida, 'w')  # abrir archivo salida
+    f.write(ret)           # escribir archivo salida
+    f.close()              # cerrar archivo salida
